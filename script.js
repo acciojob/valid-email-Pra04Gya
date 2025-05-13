241a684c683b9688dc969f1dcabd50e768cfdba3
Pra04Gya <157507407+Pra04Gya@users.noreply.github.com>
2025-05-13 21:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function validEmail(str) {
-  //your JS code here.
+function stringChop(str, size) {
+  const chunks = [];
+  size = parseInt(size); 
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.slice(i, i + size));
+  }
+  return chunks;
 }
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size));
